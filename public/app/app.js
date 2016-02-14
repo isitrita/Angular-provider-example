30e93a0da72fe28b3480ca5ec62e625f4c0b7e1e
is_it_rita <candor.cane@gmail.com>
2016-02-14 22:03:50 +0400

    Separated provider logic from config and adding configuration viewing of Version

--- public/app/app.js
+++ public/app/app.js
@@ -2,16 +2,30 @@
 
     var app = angular.module('app', []);
 
-    app.config(function($provide){
-      $provide.provider('books', function() {
-      	this.$get = function () {
-      		var appName = 'Book Logger';
-          var appDesc = 'Track which books you read.';
-      		return {
-      			appName: appName,
-            appDesc: appDesc
-      		};
-      	};
-      });
+    app.provider('books', function() {
+      this.$get = function () {
+        var appName = 'Book Logger';
+        var appDesc = 'Track which books you read.';
+
+        var version = '1.0';
+
+        if (includeVersionInTitle) {
+          appName += ' '+version;
+        }
+
+        return {
+          appName: appName,
+          appDesc: appDesc
+        };
+      };
+
+      var includeVersionInTitle = false;
+      this.setIncludeVersionInTitle  = function (value) {
+        includeVersionInTitle = value;
+      };
+    });
+
+    app.config(function(booksProvider){
+      booksProvider.setIncludeVersionInTitle(false);
     });
 }());
