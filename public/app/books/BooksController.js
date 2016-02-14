cb1707d71b96c6290deca641cbd4cfac5795fb1f
is_it_rita <candor.cane@gmail.com>
2016-02-14 18:20:17 +0400

    Injected app.$provider to app.config

---
+++ public/app/books/BooksController.js
@@ -0,0 +1,15 @@
+(function() {
+
+    angular.module('app')
+        .controller('BooksController', BooksController);
+
+
+    function BooksController(books) {
+
+        var vm = this;
+
+        vm.appName = books.appName;
+    }
+
+
+}());
