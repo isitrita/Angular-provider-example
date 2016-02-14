fdcbcf6a5d9acf30882898fa17522c58ec74ab1b
is_it_rita <candor.cane@gmail.com>
2016-02-14 22:24:13 +0400

    WIP

--- public/app/books/BooksController.js
+++ public/app/books/BooksController.js
@@ -1,15 +1,22 @@
 (function() {
 
     angular.module('app')
-        .controller('BooksController', BooksController);
+        .controller('BooksController', BooksController) ;
 
 
-    function BooksController(books) {
+    function BooksController(books, dataService) {
 
         var vm = this;
 
         vm.appName = books.appName;
-    }
 
+        vm.allBooks = dataService.getAllBooks();
+        vm.allReaders = dataService.getAllReaders();
+        //
+        // vm.getBadge = badgeService.retrieveBadge;
+        //
+        // logger.output('BooksController has been created.');
+
+    }
 
 }());
---
+++ public/app/services/dataService.js
@@ -0,0 +1,56 @@
+(function() {
+  angular.mode('app')
+           .factory('dataService', dataService);//имя нашего сервиса, функция, которая будет заассайнена на $get свойство, когда внутри будет вызываться provider. Использовать именованную функцию лучше, а не анонимную проще, потому что это проще мейнтейнить.
+  function dataService(){
+    return {
+      getAllBooks: getAllBooks,
+      getAllReaders: getAllReaders
+    };
+
+    function getAllBooks() {
+      return [
+        {
+            book_id: 2,
+            title: "The Things They Carried",
+            author: "Tim O'Brien",
+            year_published: "1990"
+        },
+        {
+            book_id: 3,
+            title: "Invisible Man",
+            author: "Ralph Ellison",
+            year_published: "1952"
+        },
+        {
+            book_id: 57,
+            title: "Advanced OS X Programming",
+            author: "Mark Dalrymple",
+            year_published: "2010"
+        }
+      ];
+    }
+
+    function getAllReaders() {
+      return [
+        {
+          reader_id: 1,
+          name: 'Lady in Red',
+          weeklyReadingGoal: 315,
+          totalMinutesRead: 5600
+        },
+        {
+          reader_id: 2,
+          name: 'Lady in Blue',
+          weeklyReadingGoal: 240,
+          totalMinutesRead: 3002
+        },
+        {
+          reader_id: 4,
+          name: 'Man in Black',
+          weeklyReadingGoal: 130,
+          totalMinutesRead: 630
+        }
+      ];
+    }
+  }
+}());
